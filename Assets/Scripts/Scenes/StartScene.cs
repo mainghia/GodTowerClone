e004b182e9547354804df7833b03b8dc4e05897d
nghia <nghia@mmj.vn>
2017-02-18 21:41:26 +0700

    Start Scene + Level 1 (Completed) + Level 2 (Incomplete).

---
+++ Assets/Scripts/Scenes/StartScene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartScene : MonoBehaviour {
+
+	public void OnBeginButtonClicked(){
+		TKSceneManager.ChangeScene("level 1");
+	}
+
+	public void OnContinueButtonClicked(){
+		// TODO: continue button function
+	}
+}
