e632f70228741b0a7e7027574d9042a80c6dc8e5
nghia <nghia@mmj.vn>
2017-02-20 22:22:03 +0700

    Level 4 completed

---
+++ Assets/Scripts/Level4BarController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Level4BarController : MonoBehaviour,EventTrigger {
+
+	private Image image;
+	private Color imageOriginalColor;
+
+	void Awake(){
+		image = GetComponent<Image> ();
+		imageOriginalColor = image.color;
+	}
+
+	public void OnMouseEnter(){
+		image.color = Color.clear;
+	}
+
+	public void OnMouseExit(){
+		image.color = imageOriginalColor;
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
