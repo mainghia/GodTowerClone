d8b36001b07c2bd369b64a96b8b35372d91c5e7a
mainghia95 <misugi296@gmail.com>
2017-03-10 23:21:07 +0700

    Skippy Monkey lam` theo Video Tut tren lop

---
+++ SkippyMonkey/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Lean.Touch;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(Animator))]
+
+public class PlayerController : MonoBehaviour {
+	public float runspeed;
+	public float jumpspeed;
+
+	private Rigidbody2D rgBody;
+	private Collider2D coll;
+	private Animator anim;
+
+	private void Awake(){
+		rgBody = GetComponent<Rigidbody2D> ();
+		coll = GetComponent<Collider2D> ();
+		LeanTouch.OnFingerTap += OnFingerTap;
+		anim = GetComponent<Animator> ();
+	}
+
+	private void OnDestroy(){
+		LeanTouch.OnFingerTap -= OnFingerTap;
+	}
+
+	private void OnFingerTap(LeanFinger finger){
+		anim.SetBool ("IsGrounded", false);
+		rgBody.velocity = new Vector2 (rgBody.velocity.x, jumpspeed);
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision){
+		anim.SetBool ("IsGrounded", true);
+	}
+
+	private void Update (){
+		rgBody.velocity = new Vector2 (runspeed, rgBody.velocity.y);
+		if (transform.position.x > 320) {
+			transform.Translate (new Vector2 (-640, 0));
+		}
+	}
+
+}
---
+++ SkippyMonkey/Assets/Scripts/SceneScript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Lean.Touch;
+
+public class SceneScript : MonoBehaviour {
+    public void OnStartButtonTapped()
+    {
+        TKSceneManager.ChangeScene("Gameplay Scene");
+    }
+}
